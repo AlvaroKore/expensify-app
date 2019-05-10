b654f74d45e7d03ece644f444524f3d31d4c14b7
Alvaro Rene <alvaro.kom@gmail.com>
2019-05-09 19:20:28 -0500

    added startSetExpenses reducer

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -67,3 +67,13 @@ test('should not edit if expense not found', () => {
 	const state = expensesReducer(expenses, action);
 	expect(state).toEqual(expenses);
 });
+
+test('should set expenses', () => {
+	const action = {
+		type: 'SET_EXPENSES',
+		expenses
+	};
+
+	const state = expensesReducer(undefined, action);
+	expect(state).toEqual(expenses);
+});
